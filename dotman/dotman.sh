a8d542c9aa60ebf54123547c7b0a3dc6d01becd9
rseld <184547417+rseld@users.noreply.github.com>
2026-08-10 22:28:02 +0200

    dotman for config files

--- dotman/dotman.sh
+++ dotman/dotman.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-rsync -a ~/.bashrc ~/.dotfiles
+rsync -av ~/.bashrc ~/.dotfiles
+rsync -av ~/.inputrc ~/.dotfiles
+rsync -av ~/.Xresources ~/.dotfiles
+rsync -av ~/.xinitrc ~/.dotfiles
+rsync -av ~/.config/tmux ~/.dotfiles
