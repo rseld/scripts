bb40e076c3d552757285f376b59b4e2b9ee2096a
rseld <184547417+rseld@users.noreply.github.com>
2026-09-05 20:44:17 +0200

    attempt at using dbus for event driven stuff

---
+++ spotify/spotify-listener.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SPOTIFY_NAME="org.mpris.MediaPlayer2.spotify"
+
+monitor_fifo=$(mktemp -u)
+mkfifo "$monitor_fifo"
+
+cleanup() {
+    kill "$monitor_pid" 2>/dev/null
+    rm -f "$monitor_fifo"
+}
+trap cleanup EXIT
+
+busctl --user monitor --json=short \
+    --match "type=signal,interface=org.freedesktop.DBus.Properties,member=PropertiesChanged,path=/org/mpris/MediaPlayer2,sender=${SPOTIFY_NAME}" \
+    > "$monitor_fifo" &
+monitor_pid=$!
+
+while read -r line; do
+    if busctl --user call "$SPOTIFY_NAME" /org/mpris/MediaPlayer2 \
+        org.freedesktop.DBus.Properties GetAll s "org.mpris.MediaPlayer2.Player" &>/dev/null; then
+    pkill -RTMIN+8 i3blocks
+    fi
+done < "$monitor_fifo"
---
+++ spotify/spotify-supervisor.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+SPOTIFY_NAME="org.mpris.MediaPlayer2.spotify"
+LISTENER_UNIT="spotify-listener.service"
+
+monitor_fifo=$(mktemp -u)
+mkfifo "$monitor_fifo"
+
+cleanup() {
+    kill "$monitor_pid" 2>/dev/null
+    rm -f "$monitor_fifo"
+}
+trap cleanup EXIT
+
+busctl --user monitor --json=short \
+    --match "type=signal,interface=org.freedesktop.DBus,member=NameOwnerChanged,arg0=${SPOTIFY_NAME}" \
+    > "$monitor_fifo" &
+monitor_pid=$!
+
+if busctl --user call org.freedesktop.DBus /org/freedesktop/DBus org.freedesktop.DBus GetNameOwner s "${SPOTIFY_NAME}" &>/dev/null; then
+    systemctl --user start "$LISTENER_UNIT"
+fi
+
+while read -r line; do
+#!
+    if [[ $line =~ \"data\":\[\"([^\"]*)\",\"([^\"]*)\",\"([^\"]*)\"\] ]]; then
+        new_owner="${BASH_REMATCH[3]}"
+        if [[ -n "$new_owner" ]]; then
+            systemctl --user start "$LISTENER_UNIT"
+        else
+            systemctl --user stop "$LISTENER_UNIT"
+        fi
+    fi
+done < "$monitor_fifo"
+
